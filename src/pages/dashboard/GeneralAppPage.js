fc1f73de4783201b082b7043954bdc92f79ad2b8
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-05-18 07:40:06 -0300

    Update the home page

--- src/pages/dashboard/GeneralAppPage.js
+++ src/pages/dashboard/GeneralAppPage.js
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 
 // @mui
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 
 // auth
 
@@ -10,6 +10,7 @@ import { Container, Grid } from '@mui/material';
 // components
 import { useSettingsContext } from '../../components/settings';
 // sections
+import AnalyticsWidgetSummary from '../../sections/@dashboard/general/analytics/AnalyticsWidgetSummary';
 
 // assets
 
@@ -26,9 +27,35 @@ export default function GeneralAppPage() {
       </Helmet>
 
       <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Olá, bem vindo de volta.
+        </Typography>
+
         <Grid container spacing={3}>
-          <Grid item xs={12} md={4}>
-            <h1>1</h1>
+          <Grid item xs={12} sm={4}>
+            <AnalyticsWidgetSummary
+              title="Negócios neste mês"
+              total={1723315}
+              icon="material-symbols:thumb-up-rounded"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <AnalyticsWidgetSummary
+              title="Vendas neste mês"
+              total={714000}
+              color="warning"
+              icon="material-symbols:account-balance-wallet"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <AnalyticsWidgetSummary
+              title="Negócios abertos"
+              total={234}
+              color="error"
+              icon="material-symbols:shopping-bag"
+            />
           </Grid>
 
           <Grid item xs={12} md={4}>
